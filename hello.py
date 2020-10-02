dc16e21314a5628f9ffd0f9f058f72bb5db2bf91
PuaChris <chris.pua@mail.utoronto.ca>
2020-10-02 01:18:28 -0400

    Added new email form that displays only UofT emails from user

--- hello.py
+++ hello.py
@@ -2,15 +2,26 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+import re
 from datetime import datetime
 
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import Required, Email
+
+def validate_uoft_email_domain(email):
+  domain = re.search("@[\w.]+", email)
+  if domain.group() == "@mail.utoronto.ca":
+    return True
+  else:
+    return False
 
 class NameForm(Form):
   name = StringField('What is your name?', validators=[Required()])
-  submit = SubmitField('Submit')
+  email = EmailField('What is your UofT Email address?', validators=[Required(), Email()])
+  submit = SubmitField('Submit')  
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -21,14 +32,30 @@ moment = Moment(app)
 @app.route('/', methods=['GET','POST'])
 def index():
   name = None
+  email = None
+  isUoftEmail = False
   form = NameForm()
+  
+  
   if form.validate_on_submit():
     old_name = session.get('name')
+    old_email = session.get('email')
+    
     if old_name is not None and old_name != form.name.data:
       flash('Looks like you changed your name!')
+    
+    if old_email is not None and old_email != form.email.data:
+      flash('Looks like you changed your email!')
+
+    isUoftEmail = validate_uoft_email_domain(form.email.data)
+      
     session['name'] = form.name.data
+    session['email'] = form.email.data
+    session['isUoftEmail'] = isUoftEmail
+    
     return redirect(url_for('index'))
-  return render_template('index.html', form=form, name=session.get('name'))
+  
+  return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), isUoftEmail = session.get('isUoftEmail'))
 
 @app.route('/user/<name>')
 def user(name):
